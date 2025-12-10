6179ffcb4c100e7cacb5162e79cf43321b916f5f
melih-ucgun <melihucgun@gmail.com>
2025-12-10 22:38:22 +0300

    Project : Neon Genesis

--- scripts/interactions/etkilesimli_kutu.gd
+++
@@ -1,6 +0,0 @@
-extends StaticBody3D
-
-# Oyuncu "E" tuşuna basınca RayCast bu fonksiyonu çalıştıracak
-func etkilesim_yap():
-	print("Kutuya dokunuldu! Güle güle kutu...")
-	queue_free() # Kutuyu sahneden siler
